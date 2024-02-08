96e3d73a852274996ca3915bb71d4b77226a7289
muktadulislam <bsse1215@iit.du.ac.bd>
2024-02-08 16:27:31 +0600

    ADD: add product grid

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Hello World</h1>
+      {/* <div className="basis-1/2 space-x-3">
+        <div className="size-40 bg-red-500"></div>
+        <div className="size-40 bg-red-500"></div>
+        <div className="size-40 bg-red-500"></div>
+        <div className="size-40 bg-red-500"></div>
+      </div> */}
+      <div className="grid grid-cols-3 gap-4 content-normal sm:grid-cols-3 md:grid-cols-4">
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div>
+        <div className="w-100% h-72 bg-red-500">Data</div> 
+        <div className="w-100% h-72 bg-red-500">Data</div>
+      </div>
+
     </>
   );
 }
